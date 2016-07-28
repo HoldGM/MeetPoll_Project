9e106bd0664375e66f8a034c7482e71473ac4888
otisbrower <otisbrower@yahoo.com>
2016-07-28 12:47:02 -0500

    working on list of things

---
+++ MeetPoll/app/src/main/java/odb234/meetpoll/VoteListActivity.java
@@ -0,0 +1,13 @@
+package odb234.meetpoll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VoteListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vote_list);
+    }
+}
